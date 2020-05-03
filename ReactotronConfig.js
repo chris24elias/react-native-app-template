6a891bef0f6310989035cc7e2f206e4c7afd075d
Christopher <chris24elias@gmail.com>
2020-05-03 14:27:40 -0400

    installed reactotron plugin for flipper

--- ReactotronConfig.js
+++ ReactotronConfig.js
@@ -1,6 +1,7 @@
-import Reactotron, { trackGlobalErrors } from "reactotron-react-native";
-import AsyncStorage from "@react-native-community/async-storage";
-import { reactotronRedux } from "reactotron-redux";
+import Reactotron, {trackGlobalErrors} from 'reactotron-react-native';
+import AsyncStorage from '@react-native-community/async-storage';
+import {reactotronRedux} from 'reactotron-redux';
+import ReactotronFlipper from 'reactotron-react-native/dist/flipper';
 
 // Reactotron.setAsyncStorageHandler(AsyncStorage) // AsyncStorage would either come from `react-native` or `@react-native-community/async-storage` depending on where you get it from
 //   .configure() // controls connection & communication settings
@@ -12,17 +13,18 @@ import { reactotronRedux } from "reactotron-redux";
 Reactotron.clear();
 
 const reactotron = Reactotron.setAsyncStorageHandler(AsyncStorage) // AsyncStorage would either come from `react-native` or `@react-native-community/async-storage` depending on where you get it from
-    .configure({
-        name: "React Native Demo",
-    })
-    .useReactNative({
-        networking: {
-            // optionally, you can turn it off with false.
-            ignoreUrls: /symbolicate/,
-        },
-    }) // add all built-in react native plugins
-    .use(reactotronRedux()) // plus some custom made plugin.
-    .use(trackGlobalErrors())
-    .connect();
+  .configure({
+    name: 'React Native Demo',
+    createSocket: (path) => new ReactotronFlipper(path),
+  })
+  .useReactNative({
+    networking: {
+      // optionally, you can turn it off with false.
+      ignoreUrls: /symbolicate/,
+    },
+  }) // add all built-in react native plugins
+  .use(reactotronRedux()) // plus some custom made plugin.
+  .use(trackGlobalErrors())
+  .connect();
 
 export default reactotron;
