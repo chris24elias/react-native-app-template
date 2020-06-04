78b1a13d818b7828af7883469ca324f8655f8d54
Christopher <chris24elias@gmail.com>
2020-06-04 12:52:59 -0400

    added some test comments for future use

--- src/Store/model/index.tsx
+++ src/Store/model/index.tsx
@@ -12,3 +12,30 @@ const model: StoreModel = {
 };
 
 export default model;
+
+// TEST
+
+// export interface ObjectWithId {
+//   id: string;
+// }
+
+// export interface DataModel<DataItem extends ObjectWithId> {
+//   data: { [key: string]: DataItem };
+//   ids: Select<DataModel<DataItem>, string[]>;
+//   fetch: Thunk<DataModel<DataItem>>;
+//   fetched: Action<DataModel<DataItem>, DataItem[]>;
+// }
+
+// export const dataModel = <Items extends ObjectWithId>(
+//   endpoint: () => Promise<Items[]>
+// ): DataModel<Items> => ({
+//   data: {},
+//   ids: select(state => Object.keys(state.data)),
+//   fetched: (state, items) => {
+//     state.data = items;
+//   },
+//   fetch: thunk(async (actions, payload) => {
+//     const data = await endpoint();
+//     actions.fetched(data);
+//   })
+// });
